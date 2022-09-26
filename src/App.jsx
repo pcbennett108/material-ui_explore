47f8638f19c4ee8502f3b185378d6ce7bc9bea90
pcbennett108 <pcbennett42@gmail.com>
2022-09-26 17:34:56 -0600

    set theme, add main section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {
   Typography,
   AppBar,
+  Button,
   Card,
   CardActions,
   CardContent,
@@ -12,17 +13,68 @@ import {
   Container,
 } from "@mui/material";
 import { PhotoCamera } from "@mui/icons-material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme();
 
 const App = () => {
   return (
     <>
-      <CssBaseline />
-      <AppBar position="relative">
-        <Toolbar>
-          <PhotoCamera />
-          <Typography variant="h6">Photo Album</Typography>
-        </Toolbar>
-      </AppBar>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="relative">
+          <Toolbar>
+            <PhotoCamera sx={{ mr: 2 }} />
+            <Typography variant="h6">Photo Album</Typography>
+          </Toolbar>
+        </AppBar>
+        <main>
+          <div
+            sx={{
+              bgcolor: "background.paper",
+              pt: 8,
+              pb: 6,
+            }}
+          >
+            <Container maxWidth="sm">
+              <Typography
+                variant="h2"
+                align="center"
+                color="textPrimary"
+                gutterBottom
+              >
+                Photo Album
+              </Typography>
+              <Typography
+                variant="h5"
+                align="center"
+                color="textSecondary"
+                paragraph
+              >
+                Hello everyone. This is a photo album made with React and
+                Material UI. The development of synthetic acoustic technologies
+                and the creation of sound art have led to a consideration of the
+                possibilities of a sound-image made up of irreducible phonic
+                substance beyond linguistic or musicological analysis.
+              </Typography>
+              <div>
+                <Grid container spacing={2} justify="center">
+                  <Grid item>
+                    <Button variant="contained" color="primary">
+                      View my images
+                    </Button>
+                  </Grid>
+                  <Grid item>
+                    <Button variant="outlined" color="primary">
+                      Get image details
+                    </Button>
+                  </Grid>
+                </Grid>
+              </div>
+            </Container>
+          </div>
+        </main>
+      </ThemeProvider>
     </>
   );
 };
