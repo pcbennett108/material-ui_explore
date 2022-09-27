9f42e1fb9e5c4609252a8b7148d3f147fcd2456b
pcbennett108 <pcbennett42@gmail.com>
2022-09-26 18:17:22 -0600

    add cards

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,8 @@ import {
 import { PhotoCamera } from "@mui/icons-material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 const theme = createTheme();
 
 const App = () => {
@@ -73,6 +75,49 @@ const App = () => {
               </div>
             </Container>
           </div>
+          <Container sx={{ py: 8 }} maxWidth="md">
+            <Grid container spacing={4}>
+              {cards.map((card) => (
+                <Grid item key={card} xs={12} sm={6} md={4}>
+                  <Card
+                    sx={{
+                      height: "100%",
+                      display: "flex",
+                      flexDirection: "column",
+                    }}
+                  >
+                    <CardMedia
+                      component="img"
+                      sx={{
+                        // 16:9
+                        pt: "56.25%",
+                      }}
+                      image="https://source.unsplash.com/random"
+                      title="Image Title"
+                      alt="random"
+                    />
+                    <CardContent sx={{ flexGrow: 1 }}>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        Heading
+                      </Typography>
+                      <Typography>
+                        This is a media card. This section will describe the
+                        content.
+                      </Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small" color="primary">
+                        View
+                      </Button>
+                      <Button size="small" color="primary">
+                        Edit
+                      </Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          </Container>
         </main>
       </ThemeProvider>
     </>
